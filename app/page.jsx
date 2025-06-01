71cfc8d3d4de8c7ec0678f840fecd067d15b4548
Rafiu Ayomide <rafiuayomide1811@gmail.com>
2025-06-01 17:28:22 +0100

    Add initial project files, including components, assets, and configuration

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+"use client"
+import { motion } from "framer-motion"
+
+const Home = () => {
+  return (
+    <motion.section 
+      initial={{ opacity: 0 }} 
+      animate={{
+        opacity: 1, 
+        transition: { delay: 1, duration: 0.4, ease: "easeInOut" } 
+      }} >
+        home
+    </motion.section>
+  )
+}
+
+export default Home;
