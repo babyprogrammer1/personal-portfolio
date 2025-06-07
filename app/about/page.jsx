acca4a373efc93b303996902400ade11cb3b92e0
Rafiu Ayomide <rafiuayomide1811@gmail.com>
2025-06-07 14:47:19 +0100

    feat: add Blob and Socials components to About section for enhanced visuals and interactivity

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -9,6 +9,8 @@ import Testimonials from "@/components/Testimonials";
 import Info from "@/components/Info";
 import Journey from "@/components/Journey";
 import Skills from "@/components/Skills";
+import Blob from "@/components/Blob";
+import Socials from "@/components/Socials";
 
 
 
@@ -25,13 +27,32 @@ const About = () => {
       <div className="container mx-auto px-0">
         <div className="flex flex-col xl:flex-row items-center gap-24 w-full h-[680px]">
           {/* img & social icon */}
-          <div className="hidden xl:flex w-full h-full pt-14 max-w-[438px] relative bg-pink-50/10">
-            1
+          <div className="hidden xl:flex flex-col w-full h-full pt-14 max-w-[430px] relative ">
+            <Image
+              src="/assets/avatar.png"
+              width={320}
+              height={496}
+              alt=""
+              className="z-20 relative"
+            />
+            {/* overlay */}
+            <div className="w-full h-[60px] absolute left-0 top-[420px] right-0 bg-gradient-to-t from-primary 
+            via-primary/90 z-30"></div>
+
+            {/* blob */}
+            <div className="absolute top-[80px] -left-[80px] z-10">
+              <Blob containerStyles="w-[420px] h-[420px]"/>
+            </div>
+
+            {/* socials */}
+            <Socials containerStyles="flex gap-4 z-40 w-max transform translate-x-[30px]"
+              iconStyles="w-[48px] h-[48px] text-[22px] text-accent hover:text-accent-hover transition-all flex items-center
+            justify-center rounded-full cursor-pointer"/>
           </div>
           {/* scroll area */}
-          <ScrollArea className="w-full h-[680px]">
+          <ScrollArea className="w-full h-[680px] ">
             <div>
-              <div className="flex items-center gap-3 mb-4">
+              <div className="flex items-center gap-3 mb-4 xl:mt-12">
                 <div className="w-2 h-2 bg-accent rounded-full"></div>
                 <p>About me</p>
               </div>
