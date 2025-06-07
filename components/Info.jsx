3f46e81568cd503f540d9ed584e1dea3a7294aa7
Rafiu Ayomide <rafiuayomide1811@gmail.com>
2025-06-07 14:45:23 +0100

    feat: update Info component with contact details and icons; enhance layout for better presentation

--- components/Info.jsx
+++ components/Info.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import {
+  HiOutlineMail,
+  HiOutlinePhone,
+  HiOutlineUser,
+  HiOutlineLocationMarker
+} from "react-icons/hi"
 
 const Info = () => {
   return (
-    <div>Info</div>
+    <div className="flex flex-col gap-6 md:gap-10">
+      <div className="flex flex-col md:flex-row gap-4">
+        <div className="w-[280px] flex items-start gap-4">
+          <HiOutlineUser className="text-2xl mt-1 text-accent" />
+          <div>
+            <p className="text-lg">Date of Birth</p>
+            <p>18th of November</p>
+          </div>
+        </div>
+        <div className="w-[280px] flex items-start gap-4">
+          <HiOutlineMail className="text-2xl mt-1 text-accent" />
+          <div>
+            <p className="text-lg">Email Address</p>
+            <p>rafiuayomide1811@gmail.com</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col md:flex-row gap-4">
+        <div className="w-[280px] flex items-start gap-4">
+          <HiOutlinePhone className="text-2xl mt-1 text-accent" />
+          <div>
+            <p className="text-lg">Phone</p>
+            <p>+234 816 3384 651</p>
+          </div>
+        </div>
+        <div className="w-[280px] flex items-start gap-4">
+          <HiOutlineLocationMarker className="text-2xl mt-1 text-accent" />
+          <div>
+            <p className="text-lg">Location</p>
+            <p>Ilorin, Nigeria.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
