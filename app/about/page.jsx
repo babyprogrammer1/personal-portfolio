fd30afa6fc9b7ebdc76841cd4c13f163f39a7e6a
Rafiu Ayomide <rafiuayomide1811@gmail.com>
2025-06-11 18:05:32 +0100

    feat: refine layout and spacing in About section for improved aesthetics

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -24,10 +24,10 @@ const About = () => {
       }}
       className="h-screen flex items-center"
     >
-      <div className="container mx-auto px-0">
-        <div className="flex flex-col xl:flex-row items-center gap-24 w-full h-[680px]">
+      <div className="container mx-auto px-2 md:px-4 xl:px-0">
+        <div className="flex flex-col xl:flex-row items-center gap-10 xl:gap-24 w-full xl:h-[680px]">
           {/* img & social icon */}
-          <div className="hidden xl:flex flex-col w-full h-full pt-14 max-w-[430px] relative ">
+          <div className="hidden xl:flex flex-col w-full h-full pt-10 max-w-[380px] relative ">
             {/*<Image
               src="/assets/avatar.png"
               width={320}
@@ -36,34 +36,31 @@ const About = () => {
               className="z-20 relative"
             />*/}
             {/* overlay */}
-            <div className="w-full h-[60px] absolute left-0 top-[420px] right-0 bg-gradient-to-t from-primary 
-            via-primary/90 z-30"></div>
+            <div className="w-full h-[50px] absolute left-0 top-[340px] right-0 bg-gradient-to-t from-primary via-primary/90 z-30"></div>
 
             {/* blob */}
-            <div className="absolute top-[80px] -left-[80px] z-10">
-              <Blob containerStyles="w-[420px] h-[420px]"/>
+            <div className="absolute top-[60px] -left-[60px] z-10">
+              <Blob containerStyles="w-[320px] h-[320px]"/>
             </div>
 
             {/* socials */}
-            <Socials containerStyles="flex gap-4 z-40 w-max transform translate-x-[30px]"
-              iconStyles="w-[48px] h-[48px] text-[22px] text-accent hover:text-accent-hover transition-all flex items-center
-            justify-center rounded-full cursor-pointer"/>
+            <Socials containerStyles="flex gap-4 z-40 w-max translate-x-[20px]"
+              iconStyles="w-[40px] h-[40px] text-[20px] text-accent hover:text-accent-hover transition-all flex items-center justify-center rounded-full cursor-pointer"/>
           </div>
           {/* scroll area */}
-          <ScrollArea className="w-full h-[680px] xl:mt-[60px] xs:mt-[50px] sm:mt-[60px] xs:h-screen">
+          <ScrollArea className="w-full h-[600px] xl:mt-[40px] xs:mt-[30px] sm:mt-[40px] xs:h-screen">
             <div>
-              <div className="flex items-center lg:justify-start gap-3 mb-4 xl:mt-12 xs:mt-7">
+              <div className="flex items-center lg:justify-start gap-3 mb-4 xl:mt-10 xs:mt-5">
                 <div className="w-2 h-2 bg-accent rounded-full"></div>
                 <p>About me</p>
               </div>
               <h2 className="h2 mb-6">
                 <span className="text-accent">Zoe</span> R.
               </h2>
-              <p className="max-w-[540px] mb-12">
-                I focus on crafting visually stunning, user-friendly web experiences that not only look great but also function
-                seamlessly, ensuring every detail is carefully designed and executed.
+              <p className="max-w-[480px] mb-10 text-base xl:text-lg">
+                I focus on crafting visually stunning, user-friendly web experiences that not only look great but also function seamlessly, ensuring every detail is carefully designed and executed.
               </p>
-              <div className="flex flex-col items-start gap-16 xs:gap-6">
+              <div className="flex flex-col items-start gap-10 xs:gap-5">
                 <Stats/>
                 <Testimonials/>
                 <Info/>
