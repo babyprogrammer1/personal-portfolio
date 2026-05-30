ac0f15d0459116aa36eecf0edff4c28ed876e541
Rafiu Ayomide Aishat <rafiuayomide1811@gmail.com>
2026-05-30 14:40:29 +0100

    fix: make services, work and about page responsive

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -25,7 +25,7 @@ const About = () => {
       }}
       className="h-screen flex items-center"
     >
-      <div className="container mx-auto px-2 md:px-4 xl:px-0 sm:mt-[120px] md:mt-[80px]">
+      <div className="container mx-auto px-2 md:px-4 xl:px-0 sm:mt-[50px] md:mt-[40px]">
         <div className="flex flex-col xl:flex-row items-center gap-10 xl:gap-24 w-full xl:h-[680px]">
           {/* img & social icon */}
           <div className="hidden xl:flex flex-col w-full h-full pt-10 max-w-[380px] relative ">
