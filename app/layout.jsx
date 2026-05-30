773df1dc26394abfbf07f53e79c699e314c079aa
Rafiu Ayomide Aishat <rafiuayomide1811@gmail.com>
2026-05-30 14:06:05 +0100

    fix: make shared layout responsive

--- app/layout.jsx
+++ app/layout.jsx
@@ -22,7 +22,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={` ${dmSans.variable} antialiased relative`}>
+      <body className={` ${dmSans.variable} antialiased overflow-hidden relative`}>
         <Gradient />
         <RectangleTransition />
         <PageTransition>
